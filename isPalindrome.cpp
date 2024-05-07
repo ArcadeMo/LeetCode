f6a3bbbf6142a5868aaf92b0d4e675fb04de8575
Mohanad <mo.arcade@gmail.com>
2024-05-07 16:45:57 -0400

    Alphanumeric fix to isPalindrome

--- isPalindrome.cpp
+++ isPalindrome.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 
 using namespace std; 
 
 class Solution {
 public:
     bool isPalindrome(string s) {
+
+        // Initialize an empty string to hold an alphanumeric input
+        string alphanumeric;
+
+        // Iterate through the string
+        for (char c : s) {
+            // Check if the char is alphanumeric 
+            if (isalnum(c)) {
+                // Convert all uppercase characters to lowercase and add to the string
+                alphanumeric += tolower(c);
+            }
+        }
+
         // Initialize two pointers
         int leftPointer = 0;
-        int rightPointer = s.size() - 1;
+        int rightPointer = alphanumeric.size() - 1;
 
         // Utilize the pointers to check from both ends to the center of the string
         while (leftPointer < rightPointer) {
             // Check if the characters at the current pointer do not match
-            if (s[leftPointer] != s[rightPointer]) {
+            if (alphanumeric[leftPointer] != alphanumeric[rightPointer]) {
                 // If the characters are not identical, return false
                 return false;
             }
@@ -28,7 +42,7 @@ public:
 
 int main() {
     Solution solution;
-    string input = "racecar";
+    string input = "A man, a plan, a canal: Panama";
     bool result = solution.isPalindrome(input);
     cout << (result ? "true" : "false") << endl;
     return 0;
